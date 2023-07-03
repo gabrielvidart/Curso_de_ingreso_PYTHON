24ff95097df0654d5d34baae9a461669ae679bdb
gabriel vidart <gabrielvidart2@gmail.com>
2023-07-03 12:51:58 -0300

    ejercicio 1

--- ejercicios/01_entrada_salida/ejercicio_01/app.py
+++ ejercicios/01_entrada_salida/ejercicio_01/app.py
@@ -6,8 +6,8 @@ import customtkinter
 
 
 '''
-nombre:
-apellido:
+nombre:gabriel
+apellido:vidart
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -28,7 +28,7 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        print("esto no anda, funciona")
         
 
 if __name__ == "__main__":
