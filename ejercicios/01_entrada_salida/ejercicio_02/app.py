7f40ce428c1d5f9f1c7c5066a43f38ad5069e16b
gabriel vidart <gabrielvidart2@gmail.com>
2023-07-06 08:42:44 -0300

    añado ejercicio 2,3,4 al repositorio

--- ejercicios/01_entrada_salida/ejercicio_02/app.py
+++ ejercicios/01_entrada_salida/ejercicio_02/app.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:gabriel
+apellido:vidart
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -28,7 +28,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        edad = prompt(title="pregunta", prompt="ingrese su edad") 
+        alert(title="edad", message="tienes la edad suficiente")
         
         
     
--- ejercicios/01_entrada_salida/ejercicio_03/app.py
+++ ejercicios/01_entrada_salida/ejercicio_03/app.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:gabriel
+apellido:vidart
 ---
 Ejercicio: entrada_salida_03
 ---
@@ -34,7 +34,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = self.txt_nombre.get()
+        saludo = "hola" + " " + nombre
+        alert(title="saludo", message=saludo)
                 
     
 if __name__ == "__main__":
